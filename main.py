9420e6a35394b762bbccd9a5934b9e350ea99da4
ibrahim akrab <ibrahim.m.akrab@gmail.com>
2023-05-13 01:52:59 +0300

    changes

--- main.py
+++ main.py
@@ -32,31 +32,31 @@ keyboard.keymap = [
         KC.ESC,  KC.N1, KC.N2,   KC.N3,   KC.N4, KC.N5,                         KC.N6, KC.N7, KC.N8,   KC.N9,  KC.N0,   KC.BSPC,
         KC.TAB,  KC.Q,  KC.W,    KC.E,    KC.R,  KC.T,                          KC.Y,  KC.U,  KC.I,    KC.O,   KC.P,    KC.BSLS,
         KC.GRV,  KC.A,  KC.S,    KC.D,    KC.F,  KC.G,                          KC.H,  KC.J,  KC.K,    KC.L,   KC.SCLN, KC.QUOT,
-        KC.LCTL, KC.Z,  KC.X,    KC.C,    KC.V,  KC.B,                          KC.N,  KC.M,  KC.COMM, KC.DOT, KC.SLSH, KC.APP,
+        KC.NO,   KC.Z,  KC.X,    KC.C,    KC.V,  KC.B,                          KC.N,  KC.M,  KC.COMM, KC.DOT, KC.SLSH, KC.NO,
                         KC.LBRC, KC.RBRC,                                                      KC.MINS, KC.EQL,
                                                 KC.MO(2), KC.SPC,      KC.ENT,  KC.MO(1),
-                                                KC.CW,    KC.DEL,      KC.BSPC, RSFT,
+                                                KC.LCTL,  KC.CW,       KC.BKDL, RSFT,
                                                 KC.LALT,  KC.LGUI,     KC.PGUP, KC.PGDN
     ] ,
     [  #1
-        KC.TRNS, KC.F1, KC.F2,   KC.F3,   KC.F4, KC.F5,                         KC.F6,   KC.F7, KC.F8,   KC.F9, KC.F10, KC.TRNS,
-        KC.TRNS, KC.NO, KC.NO,   KC.NO,   KC.NO, KC.NO,                         KC.NO,   KC.NO, KC.NO,   KC.NO, KC.NO,  KC.NO,
-        KC.TRNS, KC.N1, KC.N2,   KC.N3,   KC.N4, KC.N5,                         KC.N6,   KC.N7, KC.N8,   KC.N9, KC.N0,  KC.TRNS,
-        KC.TRNS, KC.NO, KC.NO,   KC.NO,   KC.NO, KC.NO,                         KC.NO,   KC.NO, KC.NO,   KC.NO, KC.NO,  KC.TRNS,
-                        KC.NO,   KC.NO,                                                         KC.P0,   KC.PDOT,
+        KC.TRNS, KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,                         KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.TRNS,
+        KC.TRNS, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                         KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.TRNS,
+        KC.TRNS, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC,                       KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.NO,
+        KC.TRNS, KC.GRV,  KC.MINS, KC.EQL,  KC.QUOT, KC.BSLS,                       KC.NO,   KC.NO,   KC.LBRC, KC.RBRC, KC.NO,   KC.TRNS,
+                          KC.NO,   KC.NO,                                                            KC.NO,   KC.NO,
                                                 KC.TRNS, KC.TRNS,      KC.TRNS, KC.NO,
                                                 KC.TRNS, KC.TRNS,      KC.TRNS, KC.TRNS,
                                                 KC.TRNS, KC.TRNS,      KC.TRNS, KC.TRNS
     ],
     [  #2
-        KC.TRNS, KC.F11, KC.F12, KC.NO, KC.NO, KC.NO,                           KC.NO,   KC.NO,   KC.NO, KC.NO,   KC.NO, KC.NO,
+        KC.TRNS, KC.NO,  KC.NO,  KC.NO, KC.NO, KC.NO,                           KC.NO,   KC.NO,   KC.NO, KC.NO,   KC.NO, KC.NO,
         KC.NO,   KC.NO,  KC.NO,  KC.NO, KC.NO, KC.NO,                           KC.NO,   KC.NO,   KC.NO, KC.NO,   KC.NO, KC.NO,
         KC.NO,   KC.NO,  KC.NO,  KC.NO, KC.NO, KC.NO,                           KC.LEFT, KC.DOWN, KC.UP, KC.RGHT, KC.NO, KC.NO,
         KC.NO,   KC.NO,  KC.NO,  KC.NO, KC.NO, KC.NO,                           KC.NO,   KC.NO,   KC.NO, KC.NO,   KC.NO, KC.NO,
                          KC.NO,  KC.NO,                                                           KC.NO, KC.NO,
                                                     KC.NO,   KC.TRNS,      KC.TRNS, KC.TRNS,
                                                     KC.TRNS, KC.TRNS,      KC.TRNS, KC.TRNS,
-                                                    KC.HOME, KC.END,       KC.TRNS, KC.TRNS
+                                                    KC.TRNS, KC.TRNS,      KC.HOME, KC.END
     ],
     [  #3
         KC.ESC,  KC.GRV, KC.N1,   KC.N2,   KC.N3, KC.N4,                         KC.N6, KC.N7, KC.N8,   KC.N9,  KC.N0,   KC.BSPC,
